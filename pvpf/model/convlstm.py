39e2f26a58a9b05f4c142b86b7119b6fe2a94207
yotaro-shimose <yosemat.beta@gmail.com>
2021-12-16 18:50:41 +0900

    conv_lstmモデル再実装とテスト

--- pvpf/model/convlstm.py
+++ pvpf/model/convlstm.py
@@ -1,78 +1,115 @@
+from typing import List, Optional, TypedDict
+
+import tensorflow as tf
 import tensorflow.keras as keras
 from pvpf.constants import KERAS_PACKAGE_NAME
-import tensorflow as tf
 
 
-@keras.utils.register_keras_serializable(package=KERAS_PACKAGE_NAME, name="ConvLSTM")
-class ConvLSTM(keras.Model):
+class ConvLSTMBlockParam(TypedDict):
+    num_filters: int
+    kernel_size: int
+    pooling: int
+
+
+@keras.utils.register_keras_serializable(
+    package=KERAS_PACKAGE_NAME, name="ConvLSTMBlock"
+)
+class ConvLSTMBlock(keras.Model):
     def __init__(
         self,
-        num_layers: int,
         num_filters: int,
-        output_scale: float,
-        dim_cushion: int,
+        kernel_size: int,
+        pooling: int,
+        return_sequences: Optional[bool],
         *args,
-        **kwargs
+        **kwargs,
     ):
         super().__init__(*args, **kwargs)
-        self._num_layers = num_layers
         self._num_filters = num_filters
-        self._output_scale = output_scale
-        self._dim_cushion = dim_cushion
-        self._layers = list()
-        for i in range(num_layers):
-            self._layers.append(keras.layers.LayerNormalization())
-            return_sequences = i + 1 != num_layers
-            self._layers.append(
-                keras.layers.ConvLSTM2D(
-                    filters=num_filters,
-                    kernel_size=5,
-                    return_sequences=return_sequences,
-                )
-            )
-        self._pooling = keras.layers.GlobalAveragePooling2D()
-        self._cushion_dense = keras.layers.Dense(dim_cushion, activation="relu")
-        self._final_dense = keras.layers.Dense(1)
-        self._final_reshape = keras.layers.Reshape(target_shape=())
+        self._kernel_size = kernel_size
+        self._pooling = pooling
+        self._return_sequences = return_sequences
+        self._layer_norm = keras.layers.LayerNormalization()
+        self._convlstm = keras.layers.ConvLSTM2D(
+            num_filters, kernel_size, return_sequences=return_sequences, padding="same"
+        )
+        self._pooling = (
+            keras.layers.MaxPool3D(pool_size=(1, pooling, pooling))
+            if pooling is not None
+            else None
+        )
 
     def call(self, x: tf.Tensor) -> tf.Tensor:
-        for layer in self._layers:
-            x = layer(x)
-        x = self._pooling(x)
-        x = self._cushion_dense(x)
-        x = self._final_dense(x)
-        x = self._final_reshape(x)
-        x = x * self._output_scale
+        x = self._layer_norm(x)
+        x = self._convlstm(x)
+        if self._pooling is not None:
+            x = self._pooling(x)
         return x
 
     def get_config(self) -> dict:
-        config: dict = super().get_config()
-        custom_config = {
-            "num_layers": self._num_layers,
+        default = super().get_config()
+        ans = default | {
             "num_filters": self._num_filters,
-            "ouput_scale": self._output_scale,
-            "dim_cushion": self._dim_cushion,
+            "kernel_size": self._kernel_size,
+            "pooling": self._pooling,
+            "return_sequences": self._return_sequences,
         }
-        config.update(custom_config)
-        return config
+        return ans
 
 
-def build_conv_lstm() -> keras.Sequential:
-    model = keras.Sequential(
-        [
-            keras.layers.LayerNormalization(),
-            keras.layers.ConvLSTM2D(filters=16, kernel_size=5, return_sequences=True),
-            # keras.layers.LayerNormalization(),
-            # keras.layers.ConvLSTM2D(
-            #     filters=64, kernel_size=5, padding="same", return_sequences=True
-            # ),
-            keras.layers.LayerNormalization(),
-            keras.layers.ConvLSTM2D(
-                filters=64, kernel_size=5, padding="same", return_sequences=False
-            ),
-            keras.layers.GlobalAveragePooling2D(),
-            keras.layers.Dense(1),
-            keras.layers.Reshape(target_shape=()),
+@keras.utils.register_keras_serializable(
+    package=KERAS_PACKAGE_NAME, name="ConvLSTMImageEmbedder"
+)
+class ConvLSTMImageEmbedder(keras.Model):
+    def __init__(
+        self,
+        block_params: List[ConvLSTMBlockParam],
+        *args,
+        **kwargs,
+    ):
+
+        super().__init__(*args, **kwargs)
+        self._block_params = block_params
+        self._block_layers = [
+            ConvLSTMBlock(**param, return_sequences=(idx != len(block_params) - 1))
+            for idx, param in enumerate(block_params)
         ]
-    )
-    return model
+        self._flatten_layer = keras.layers.Flatten()
+
+    def call(self, x: tf.Tensor) -> tf.Tensor:
+        """spatio-temporal infomation into a single vector
+
+        Args:
+            x (tf.Tensor[B, T, W, H, F]): a spatio-temporal information
+
+        Returns:
+            tf.Tensor[B, F']: embedded infomation as a vector
+        """
+        for layer in self._block_layers:
+            x = layer(x)
+        x = self._flatten_layer(x)
+        return x
+
+    def get_config(self) -> dict:
+        default = super().get_config()
+        ans = default | {"block_params": self._block_params}
+        return ans
+
+
+@keras.utils.register_keras_serializable(
+    package=KERAS_PACKAGE_NAME, name="ConvLSTMRegressor"
+)
+class ConvLSTMRegressor(keras.Model):
+    def __init__(
+        self,
+        block_params: List[ConvLSTMBlockParam],
+    ):
+        super().__init__()
+        self._embedder = ConvLSTMImageEmbedder(block_params)
+        self._dense = keras.layers.Dense(1)
+
+    def call(self, x: tf.Tensor) -> tf.Tensor:
+        x = self._embedder(x)
+        x = self._dense(x)
+        x = tf.squeeze(x, -1)
+        return x
